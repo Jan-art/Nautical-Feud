f28e7343d0c93c2058b329c33d28265ddbfa3575
DanielPrice14 <93609347+DanielPrice14@users.noreply.github.com>
2021-11-14 12:04:41 +0000

    Proton functionality
    - Build does not work
    - Code is untested for connecting to lobby as second instance cannot be run but should work

--- Prototype 1/Assets/Scripts/LobbyManager.cs
+++ Prototype 1/Assets/Scripts/LobbyManager.cs
@@ -8,34 +8,73 @@ using Photon.Realtime;
 public class LobbyManager : MonoBehaviourPunCallbacks
 {
     [SerializeField]
-    GameObject PlayBtn;
+    GameObject MainMenu;
     [SerializeField]
     GameObject Searching;
 
     void Start()
     {
+        Debug.Log("Start running from LobbyManager");
         Searching.SetActive(false);
-        PlayBtn.SetActive(false);
+        MainMenu.SetActive(false);
         PhotonNetwork.ConnectUsingSettings();
-        Debug.Log("Passed 'ConnectUsingSettings()' method");
+        Debug.Log("Start finished from LobbyManager");
     }
 
+    //On connection to server reveals the play button to allow players to search for matches
     public override void OnConnectedToMaster()
     {
-        Debug.Log("Connected to photon on " + PhotonNetwork.CloudRegion +  " server.");
+        Debug.Log("Connected to photon on " + PhotonNetwork.CloudRegion + " server.");
         PhotonNetwork.AutomaticallySyncScene = true;
-        PlayBtn.SetActive(true);
+        MainMenu.SetActive(true);
     }
 
     public void FindMatch()
     {
         Searching.SetActive(true);
-        PlayBtn.SetActive(false);
+        MainMenu.SetActive(false);
+        PhotonNetwork.JoinRandomRoom();
+        Debug.Log("Searching for a game");
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Couldn't find room. Creating own room");
+        MakeRoom();
+    }
+
+    //Method to create a room
+    public void MakeRoom()
+    {
+        int randomRoomName = Random.Range(0, 5000);
+
+        //Makes a room for two players that can be joined by anyone
+        RoomOptions roomOptions =
+        new RoomOptions()
+        {
+            IsVisible = true,
+            IsOpen = true,
+            MaxPlayers = 2
+        };
+        PhotonNetwork.CreateRoom("RoomName_" + randomRoomName, roomOptions);
+        Debug.Log("Room created. Waiting for another player...");
+    }
+
+    //Starts the match once another player enters the room
+    public override void OnPlayerEnteredRoom(Player secondPlayer)
+    {
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 2 && PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log("Starting match");
+            PhotonNetwork.LoadLevel(1);
+        }
     }
 
     public void StopSearch()
     {
         Searching.SetActive(false);
-        PlayBtn.SetActive(true);
+        MainMenu.SetActive(true);
+        PhotonNetwork.LeaveRoom();
+        Debug.Log("Search Stopped");
     }
 }
