08f144f68fd1deaba4c68eb81fdc131ade1b2b2a
DanielPrice14 <93609347+DanielPrice14@users.noreply.github.com>
2021-12-01 15:48:43 +0000

    Multiplayer Progress
    Switches place mode successfully as long as start game overlay not active. However lacks functionality to convey where ships have been placed.
    Tile selected event attempted however raise events cannot send custom types and I couldn't figure out how to get tile info on the receiving player.
    
    Game Manager -
    Addition line 14 - constants holding event codes
    Addition line 288/317 - Raises event for ship placement being completed
    Addition line 449 - Raises event for tile selected
    Addition line 536 - Enables Photon Events

---
+++ Prototype 1_clone_0/Assets/Scripts/TileInfo.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileInfo : MonoBehaviour
+{
+    public int xPos;
+    public int zPos;
+
+    bool hit;
+
+    public SpriteRenderer sprite;
+    public Sprite[] tileTops;  // 0 = FRAME || 1 = CROSSHAIR || 2 = WATER || 3 = HIT
+                               
+    public void ActivateTop(int index, bool _hit) 
+    {
+        sprite.sprite = tileTops[index];
+
+        //COLOUR TILE
+
+        hit = _hit;
+
+    }
+
+    public void SetTilePos(int _xpos, int _zPos)
+    {
+        xPos = _xpos;
+        zPos = _zPos;
+    }
+
+    void OnMouseOver()
+    {
+        if(GameManager.instance.gameState == GameManager.GameStates.KILL) 
+        {
+            if (!hit)
+            {
+                ActivateTop(1,false);
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                //IDENTIFY CORDINATE
+                GameManager.instance.CheckShot(xPos, zPos, this);
+            }
+        }
+       
+
+    }
+
+    void OnMouseExit()
+    {
+        if(!hit)
+        {
+            ActivateTop(0,false);
+        }
+    }
+
+}
