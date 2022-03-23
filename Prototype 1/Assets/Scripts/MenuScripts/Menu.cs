5d328fa928eea58d3b60e924adeb35f1884547c8
Jan <71454686+Jan-art@users.noreply.github.com>
2022-03-23 06:21:43 +0000

    Big Changes
    - Organised asset files
    
    - Improved the Place Canvas UI ( Looks much better, but maybe will change the way images of the ships are displayed, if necessary).
    
    - Added Tutorial.Scene Slide 2 (For Advanced Mode)
    
    - Started adding button scale up animations
    
    - Started adding click sounds to the rest of the buttons.
    
    - Started ScoreBoard.Scene
    
    - Improved Win/Defeat Screens

---
+++ Prototype 1/Assets/Scripts/MenuScripts/Menu.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    
+    public void ReturnToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+        Debug.Log("Menu Scene Loaded");
+    }
+
+    public void NextSlide()
+    {
+        SceneManager.LoadScene("Tutorial2.Scene");
+        Debug.Log("Slide 2 Loaded");
+    }
+
+    public void PreviousSlide()
+    {
+          SceneManager.LoadScene("Tutorial.Scene");
+        Debug.Log("Slide 1 Loaded");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Debug.Log("Restart Scene Loaded");
+    }
+
+    public void Load(string sceneToLoad)
+    {
+        SceneManager.LoadScene(sceneToLoad);
+        Debug.Log("New Scene Loaded");
+
+    }
+
+    public void ExitGame()
+    {
+     Application.Quit();
+     Debug.Log("Game is exiting");
+    }
+}
