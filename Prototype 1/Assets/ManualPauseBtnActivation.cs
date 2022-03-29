ee18aefb09f0696009dcab2442192ae4e8819795
Jan <71454686+Jan-art@users.noreply.github.com>
2022-03-29 04:27:48 +0100

    Various Changes
    - Centred Chat Box TOGGLE to bottom of screen and set to be used by a WorldCamera Pos.
    
    - Added ManualPause Icon on top right of screen and necessary script for it's functionality.
    
    - Organised Icons & Images
    
    - Changed Arrow Icons in Tutorial Scenes.
    
    - Added PowerUp Icons to both Tutorial Scene and PowerUpBar
    
    - Added Settings options to Pause-Menu + Functionality.
    
    - Updated Build

---
+++ Prototype 1/Assets/ManualPauseBtnActivation.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManualPauseBtnActivation : MonoBehaviour
+{
+    private bool ActiveState;
+    public GameObject PauseMenu;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        PauseMenu.SetActive(false);
+        ActiveState = false;
+    }
+
+
+    public void Toggle()
+    {
+        Debug.Log("Function Toggle ran");
+        if (ActiveState == false)
+        {
+            PauseMenu.SetActive(true);
+            ActiveState = true;
+            Debug.Log("PauseMenu set to active");
+        }
+        else if (ActiveState == true)
+        {
+            PauseMenu.SetActive(false);
+            ActiveState = false;
+            Debug.Log("PauseMenu set to inactive");
+        }
+    }
+}
